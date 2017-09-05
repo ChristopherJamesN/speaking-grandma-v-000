9dfebdd20f730812f0e9830597da066368b455f1
Chris Nady <nady.christopher@gmail.com>
2017-09-05 23:26:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (phrase)
-  if (phrase.uppercase=phrase && phrase="I LOVE YOU GRANDMA!")
+  if (phrase.upcase==phrase && phrase="I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.uppercase=phrase
+  elsif phrase.upcase=phrase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
